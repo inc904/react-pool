9dc4f6a18dceaadaeb74a37b557b234282c16caa
inc904 <jay1845@aliyun.com>
2024-08-16 17:27:18 +0800

    feat✨: 鉴权路由优化

--- react-router-auth/src/views/layout.tsx
+++ react-router-auth/src/views/layout.tsx
@@ -1,20 +1,13 @@
 import { Link, Outlet } from 'react-router-dom'
-import AuthStatus from './authStatus'
+import StatusBar from './statusBar'
 
 function Layout() {
   return (
     <div>
-      <AuthStatus />
-
-      <ul>
-        <li>
-          <Link to='/'>公共页面</Link>
-        </li>
-        <li>
-          <Link to='/protected'>受保护页面</Link>
-        </li>
-      </ul>
-
+      <StatusBar />
+      <nav>
+        <Link to='/'>公共页面</Link> | <Link to='/protected'>受保护页面</Link>
+      </nav>
       <Outlet />
     </div>
   )
