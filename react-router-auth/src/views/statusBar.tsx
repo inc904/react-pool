8a2aad93e5d2fd7a3ecdb71fcd001c602e8328ce
inc904 <jay1845@aliyun.com>
2024-08-16 17:27:22 +0800

    feat✨: 鉴权路由优化

---
+++ react-router-auth/src/views/statusBar.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from './hook.tsx'
+function statusBar() {
+  let auth = useAuth()
+  let navigate = useNavigate()
+
+  if (!auth.user) {
+    return <p>还未登录！</p>
+  }
+
+  return (
+    <p>
+      欢迎 {auth.user}!{' '}
+      <button
+        onClick={() => {
+          auth.signout(() => navigate('/'))
+        }}>
+        登出
+      </button>
+    </p>
+  )
+}
+export default statusBar
